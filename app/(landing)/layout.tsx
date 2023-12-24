9ac50a81e247b67197ec8513cfc7b313e615d0e2
Brian Ho <YWHo@users.noreply.github.com>
2023-12-24 14:48:14 +1300

    add navigation bar to the top

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LandingPageNavbar from "./components/LandingPageNavbar";
+
+function LandingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="flex min-h-screen flex-col overflow-x-clip">
+      <LandingPageNavbar />
+
+      <div className="flex-grow">{children}</div>
+    </section>
+  );
+}
+
+export default LandingLayout;
