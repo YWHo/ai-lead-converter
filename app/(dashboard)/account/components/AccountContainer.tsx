ea059ccb6d5a180f1406fda8c69b2319fb05c486
Brian Ho <YWHo@users.noreply.github.com>
2024-04-05 17:03:24 +1300

    add/update UI of 'AccountContainer' client component

--- app/(dashboard)/account/components/AccountContainer.tsx
+++ app/(dashboard)/account/components/AccountContainer.tsx
@@ -1,14 +1,75 @@
-import { Account } from '@prisma/client'
-import React from 'react'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Account } from "@prisma/client";
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 interface AccountContainerProps {
   account: Account;
 }
 
 function AccountContainer({ account }: AccountContainerProps) {
+  const [username, setUsername] = useState(account.username);
+  const [isSaving, setIsSaving] = useState(false);
+  const updateUsername = async () => {
+    try {
+      setIsSaving(true);
+      const res = await axios.put("/api/account", { username });
+      if (!res.data.success) {
+        toast.error(
+          "Something went wrong saving the username. Please try again"
+        );
+        console.error("AccountContainer /api/account:\n", res.data);
+        return;
+      }
+
+      const updatedAccount = res.data.account;
+      if (updatedAccount) {
+        setUsername(updatedAccount.username);
+        toast.success("Username updated successfully!");
+      }
+    } catch (err) {
+      toast.error("Something went wrong saving the username. Please try again");
+      console.error("Something went wrong saving the username:\n", err);
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
   return (
-    <div>AccountContainer</div>
-  )
+    <div className="flex h-full w-full flex-col m-8 gap-y-4">
+      <h1 className="text-2xl font-semibold text-gray-700">Account Home</h1>
+      <hr />
+      <div className="w-fit">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          Username
+        </label>
+        <Input
+          type="text"
+          className="focus:shadow-outline w-full appearance-none rounded border"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          placeholder="Username here..."
+        />
+      </div>
+      <div className="flex flex-row gap-x-4">
+        <Button
+          variant="outline"
+          onClick={() => setUsername(account?.username || "")}
+        >
+          Cancel
+        </Button>
+        <Button disabled={isSaving} onClick={updateUsername}>
+          {isSaving ? "Saving..." : "Save"}
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default AccountContainer
+export default AccountContainer;
